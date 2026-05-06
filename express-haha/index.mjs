52733ffbd6e4435c9ff861a349d7ca44786466b9
Prem-0007 <ballapremkumar777@gmail.com>
2026-05-06 22:44:22 +0530

    modified index.mjs

--- express-haha/index.mjs
+++ express-haha/index.mjs
@@ -4,8 +4,12 @@ const app = express()
 
 
 const users = [{id:1, username:"prem", displayName:"Prem"},
-    {id:1, username:"kas", displayName:"Kas"},
-    {id:1, username:"than", displayName:"Than"}
+    {id:2, username:"kas", displayName:"Kas"},
+    {id:3, username:"than", displayName:"Than"},
+     {id:4, username:"tj", displayName:"Tj"},
+      {id:5, username:"hari", displayName:"Hari"}
+      , {id:6, username:"charan", displayName:"Charan"}
+      , {id:7, username:"manoj", displayName:"Manoj"}
  ]
 
 
@@ -14,9 +18,19 @@ app.get("/", (req, res)=> {
    res.status(201).send({msg:"hello"});
 })
 
-app.get("/api/users" , (req,res) =>{
- res.send(users)
-})
+app.get("/api/users", (req, res) => {
+    const { filter, value } = req.query;
+
+    if (!filter || !value) {
+        return res.send(users);
+    }
+
+    const result = users.filter(user =>
+        String(user[filter])?.includes(value)
+    );
+
+    return res.send(result);
+});
 
 
 app.get('/api/users/:id', (req,res) =>{
