ad8979e9f5da5aea8ecfff127e125b60689456a1
Prem-0007 <ballapremkumar777@gmail.com>
2026-05-06 08:54:26 +0530

    added updated ....

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,44 @@
 const express= require("express");
 const router = express.Router();
 
-
+router.use(logger)
 router.get('/',(req, res) => {
+   console.log(req.query.name)
  res.send("User List")
 })
 router.get('/new', (req,res) =>{
- res.send("User New Form")
+ res.render("users/new", {firstName:"Test"})
 })
 
 
 router.post('/', (req,res)=>{
-    res.send('Create User')
+  //  res.send('Create User')
+  const isValid = true
+  if(isValid){
+  users.push({firstName:req.body.firstName})
+  res.redirect(`/users/${users.length-1}`)
+  } else{
+    console.log('Error')
+    res.render('users/new', {firstName:req.body.firstName})
+  }
+ // console.log(req.body.firstName)
+ // res.send("hi");
 })
 
 
 router
 .route('/:id')
-.get((req,res)=>{  res.send(` Get the user With ID ${ req.params.id}`)})
+.get((req,res)=>{ 
+     console.log(req.user) 
+    res.send(` Get the user With ID ${ req.params.id}`)})
 .put((req,res)=>{  res.send(` Update the user With ID ${ req.params.id}`) })
 .delete((req,res)=>{ res.send(` Delete the user With ID ${ req.params.id}`) })
 
+
+const users =  [{name:" Prem"},{name:"Kas"}]
 router.param("id", (req, res, next , id) => {
-    console.log(id);
+    req.user=users[id];
+   next()
 })
 // router.get('/:id', (req,res)=>{  res.send(` Get the user With ID ${ req.params.id}`)})
 
@@ -30,6 +46,9 @@ router.param("id", (req, res, next , id) => {
 
 
 // router.delete('/:id', (req,res)=>{ res.send(` Delete the user With ID ${ req.params.id}`) })
-
+function logger(req,res,next){
+   console.log(req.originalUrl)
+   next()
+}
 
 module.exports= router;
--- server.js
+++ server.js
@@ -2,19 +2,21 @@
 const express = require("express")
 
 const app = express()
-
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+app.use(express.json())
 app.set('view engine' ,'ejs')
-
-app.get("/", (req,res) => {
-    console.log("hello")
+// app.use(logger)
+// app.get("/",  logger, (req,res) => {
+  //   console.log("hello")
    // res.send("hi")
  //  let k = res.status(500).json({message:"Error"});
   // res.download("server.js")
-  res.render("index", {text:"world"})
-})
+ //  res.render("index", {text:"world"})})
 const userRouter = require('./routes/users')
 
 app.use('/users', userRouter);
+
 
 app.listen(3000)
 
